75fb64d056a4bca6bd7ecc139cd59f981c0b78ae
@Romka <romaamzok@gmail.com>
2024-03-20 11:02:19 +0300

    задача 2_1_3 выполнена

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-public class  AnimalsCage {
+public class AnimalsCage {
 
     @Autowired
     @Qualifier("dog")
